fd546b32fb86c741b2a56da31390adab8cf22f5b
Y3llowm4n <mikegeelen2002@gmail.com>
2023-05-27 18:58:28 +0200

    Finished application
    Database connection + inserting query

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask, url_for, render_template, redirect
+from flask import Flask, url_for, render_template, redirect, request
 from views import views 
+# import pypyodbc as odbc 
+# import pandas as pd 
+# import sys
+# from credential import username, password
 
 app = Flask(__name__, static_folder='static')
-
+app.secret_key = "hello"
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
+    
--- views.py
+++ views.py
@@ -1,7 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect
+import pypyodbc as odbc
+import pandas as pd 
+import sys
+from credential import username, password
 
 views = Blueprint(__name__, "views")
 
-@views.route("/")
+@views.route("/", methods=['GET','POST'])
 def home():
+    if request.method == 'POST':
+        discount_code = request.form['discount_code']
+        try:
+                # Connect to the Azure SQL Database
+                connection_string = ('DRIVER={ODBC Driver 18 for SQL Server};Server=tcp:terraform-sql-server01.database.windows.net,1433;Database=terraform-sql-db01;Uid='+username+';Pwd='+password+';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+                conn = odbc.connect(connection_string)
+                
+                cursor = conn.cursor()
+
+                # Insert the discount code into the 'DiscountCodes' table
+                cursor.execute("INSERT INTO DiscountCodes (DiscCode) VALUES (?)", (discount_code,))
+                conn.commit()
+                flash("Code succesfully applied!", "info")
+                return redirect(request.url)
+        except Exception as e:
+                return f'An error occurred: {str(e)}'
+        finally:
+                cursor.close()
+                conn.close()     
+              
     return render_template("index.html")
