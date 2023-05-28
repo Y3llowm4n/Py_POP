66ad868c28a8ba64da2f444597f417c9a031d199
Y3llowm4n <mikegeelen2002@gmail.com>
2023-05-28 17:24:50 +0200

    remove comment

--- views.py
+++ views.py
@@ -40,26 +40,3 @@ def insert_table():
             conn.close()
 
     return render_template("index.html")
-
-# def insert_table():
-#     if request.method == 'POST':
-#         discount_code = request.form['discount_code']
-#         try:
-#                 # Connect to the Azure SQL Database
-#                 connection_string = ('DRIVER={ODBC Driver 18 for SQL Server};Server=tcp:terraform-sql-server01.database.windows.net,1433;Database=terraform-sql-db01;Uid='+username+';Pwd='+password+';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-#                 conn = odbc.connect(connection_string)
-                
-#                 cursor = conn.cursor()
-
-#                 # Insert the discount code into the 'DiscountCodes' table
-#                 cursor.execute("INSERT INTO DiscountCodes (DiscCode) VALUES (?)", (discount_code,))
-#                 conn.commit()
-#                 flash("Code succesfully applied!", "info")
-#                 return redirect(request.url)
-#         except Exception as e:
-#                 return f'An error occurred: {str(e)}'
-#         finally:
-#                 cursor.close()
-#                 conn.close()     
-              
-#     return render_template("index.html")
