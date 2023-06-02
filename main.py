caa239c5513e697a2a750cbab6639ecf3c00e397
Y3llowm4n <mikegeelen2002@gmail.com>
2023-06-02 09:55:59 +0200

    Changed port

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask, url_for, render_template, redirect, request
 from views import views 
-# import pypyodbc as odbc 
-# import pandas as pd 
-# import sys
-# from credential import username, password
+import pypyodbc as odbc
+import pandas as pd 
+import sys
+from credential import username, password
 
 app = Flask(__name__, static_folder='static')
 app.secret_key = "hello"
 app.register_blueprint(views, url_prefix="/")
 
+
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=80)
     
